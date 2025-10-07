e4a78f6e00f62ac214c19723ad8ec69d267b87de
terrence <mrgee1978@proton.me>
2025-10-06 23:10:01 -0400

    Continuing work on Views/ViewModels

---
+++ ECommerce.AvaloniaClient/ViewModels/AuthViewModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using ECommerce.Shared.Dtos.Auth.Request;
+using ECommerce.AvaloniaClient.Interfaces.Api;
+
+namespace ECommerce.AvaloniaClient.ViewModels;
+
+public partial class AuthViewModel : ObservableValidator
+{
+    
+}
